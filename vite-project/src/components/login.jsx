660bcbed1c39a33adbf64469638e7e901b22b7b0
حبيبة مصطفى محمود قناوى <143437900+Habibakenawy@users.noreply.github.com>
2024-06-14 02:41:50 +0300

    added dependencies

---
+++ vite-project/src/components/login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+import { Component } from "react";
+
+
+class Login extends Component {
+
+    render() {
+        return (
+            <div>
+            <h3>React Login Component</h3>
+        </div>
+        );
+    }
+}
+
+export default Login;
---
+++ vite-project/src/components/signin.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+import { Component } from "react";
+
+
+class Signin extends Component {
+
+    render() {
+        return (
+            <div>
+            <h3>React Signin Component</h3>
+        </div>
+        );
+    }
+}
+
+export default Signin;
